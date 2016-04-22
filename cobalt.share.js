47fd894eefd414e49108810f8205ecda9c3af308
shgz <roxane.paurobally.kristal@gmail.com>
2016-04-22 18:28:58 +0300

    Added cobalt.share.js

---
+++ cobalt.share.js
@@ -0,0 +1,39 @@
+(function(cobalt) {
+    var plugin = {
+        name: "share",
+        init: function(options) {
+            //create shortcuts
+            cobalt.shareItem = this.shareItem.bind(this);
+            if (options) {
+                cobalt.log("share.js init called with option ", options);
+                this.config(options);
+            }
+        },
+        config: function(settings) {
+            cobalt.log("share.js config called with settings ", settings);
+        },
+        shareItem: function(data, callback) {
+            this.send("share", data, function(data) {
+                cobalt.log('share.js: function callback called with data:', data, " callback:", callback);
+                if (typeof callback == 'function') {
+                    callback(data);
+                } else {
+                    cobalt.log('share.js: received callback,', callback);
+                }
+            })
+        },
+        handleEvent: function(json) {
+            cobalt.log("share.js handleEvent called", this.name, ' plugin : unknown event received :', json);
+        },
+        send: function(action, data, callback) {
+            cobalt.log("share.js send called with action :", action, " callback :", callback, " data:", data);
+            cobalt.send({
+                type: "plugin",
+                name: this.name,
+                action: action,
+                data: data,
+            }, callback);
+        }
+    };
+    cobalt.plugins.register(plugin);
+})(cobalt || {});
