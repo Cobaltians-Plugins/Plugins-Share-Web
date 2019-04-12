a432113659032489b8173d2c1fcdd12dc53e7db8
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-12 17:31:59 +0200

    sending classes instead of name

--- cobalt.share.js
+++ cobalt.share.js
@@ -1,22 +1,14 @@
 (function(cobalt) {
     var plugin = {
-        name: "share",
+        classes: {
+        	ios: 'CobaltSharePlugin',
+			android: 'io.kristal.shareplugin.SharePlugin'
+        },
         init: function() {
-            //create shortcuts
             cobalt.share = this.shareItem.bind(this);
         },
         shareItem: function(data, callback) {
-            cobalt.plugins.send(this, "share", data, function(data) {
-                cobalt.log('share.js: function callback called with data:', data, " callback:", callback);
-                if (typeof callback == 'function') {
-                    callback(data);
-                } else {
-                    cobalt.log('share.js: received callback,', callback);
-                }
-            })
-        },
-        handleEvent: function(json) {
-            cobalt.log("share.js handleEvent called", this.name, ' plugin : unknown event received :', json);
+            cobalt.plugins.send(this, "share", data, callback)
         }
     };
     cobalt.plugins.register(plugin);
