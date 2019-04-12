0111c985821fd859bb328749c1b225431a379755
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-12 15:46:53 +0200

    use cobalt.plugins.send(this,action,data,callback)

--- cobalt.share.js
+++ cobalt.share.js
@@ -6,7 +6,7 @@
             cobalt.share = this.shareItem.bind(this);
         },
         shareItem: function(data, callback) {
-            this.send("share", data, function(data) {
+            cobalt.plugins.send(this, "share", data, function(data) {
                 cobalt.log('share.js: function callback called with data:', data, " callback:", callback);
                 if (typeof callback == 'function') {
                     callback(data);
@@ -17,15 +17,6 @@
         },
         handleEvent: function(json) {
             cobalt.log("share.js handleEvent called", this.name, ' plugin : unknown event received :', json);
-        },
-        send: function(action, data, callback) {
-            cobalt.log("share.js send called with action :", action, " callback :", callback, " data:", data);
-            cobalt.send({
-                type: "plugin",
-                name: this.name,
-                action: action,
-                data: data,
-            }, callback);
         }
     };
     cobalt.plugins.register(plugin);
