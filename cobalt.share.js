fab9cfed45ced72eb406920de6c02074671f58f6
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:21:58 +0200

    ready to handle event

--- cobalt.share.js
+++ cobalt.share.js
@@ -1,16 +1,22 @@
 (function(cobalt) {
-    var plugin = {
-        classes: {
-        	ios: 'CobaltSharePlugin',
-			android: 'io.kristal.shareplugin.SharePlugin'
-        },
-        init: function() {
-            cobalt.share = this.shareItem.bind(this);
-        },
-        shareItem: function(data, callback) {
-            cobalt.plugins.send(this, "share", data, callback)
-        }
-    };
-    cobalt.plugins.register(plugin);
+  var plugin = {
+    classes: {
+      ios: 'CobaltSharePlugin',
+      android: 'io.kristal.shareplugin.SharePlugin'
+    },
+    init: function() {
+      cobalt.share = this.shareItem.bind(this);
+    },
+    shareItem: function(data, callback) {
+      this.onShareResult = callback;
+      cobalt.plugins.send(this, "share", data)
+    },
+    handleEvent: function(json) {
+      if (typeof this.onShareResult === 'function') {
+        this.onShareResult(json.data)
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 })(cobalt || {});
 
